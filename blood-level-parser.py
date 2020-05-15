d290e8c1707d39c8eb7060d17b43d653b04f5fa9
Roman Zahorskyi <zagorsky.roman@gmail.com>
2020-05-15 12:20:46 +0300

    Added the parser method bypassing 403 error code
    Headers were included to avoid 403 error, when the site recognises a parser instead of a web-browser user.

--- blood-level-parser.py
+++ blood-level-parser.py
@@ -1,22 +1,36 @@
-import requests
+from bs4 import BeautifulSoup
+from functools import lru_cache
 import cfg
+import requests
 
 
 class Parser:
     """Parses the page and saves the data that has been collected into the mysqldb"""
 
-    def __init__(self, url, db_credentials):
+    def __init__(self, url: str, db_credentials: tuple):
         self.page_url = url
         self.mysql_credentials = db_credentials
 
-    def parse_the_page(self) -> dict:
-        # TODO: look up the model used in covid parser and collect the data into a dict
+    @lru_cache(maxsize=128)
+    def parse_the_page(self):
+        page_headers = {
+            'User-Agent' :
+            "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) snap Chromium/81.0.4044.138 Chrome/81.0.4044.138 Safari/537.36"
+        } # headers are necessary to emulate a 'live user' connection
+        open_url = requests.get(self.page_url, headers=page_headers).text
+        soup = BeautifulSoup(open_url, 'lxml')
+        print(soup)
+        return soup
+
+    def clear_html_tags(self, tags: dict) -> dict:
         # Search inside <div class="vc_row wpb_row vc_inner vc_row-fluid">
         # Two separate columns have similar structure - data can be collected through indexing of elements
+
         pass
 
+
     def save_to_mysqldb(self):
-        # TODO: use the same approach as in covid, but with string concatenation, hide db credentials
+        # TODO: use the same approach as in covid, but with string concatenation, hide db credentials inside cfg module
         pass
 
 
@@ -27,20 +41,22 @@ class DataFrame:
 
 
 class TelegramBot:
-    # Link to the newly-generated bot: t.me/donor_notify_bot
-    telegram_bot_token = cfg.token
+
+    def __init__(self):
+        # Link to the newly-generated bot: t.me/donor_notify_bot
+        self.telegram_bot_token = cfg.token
 
     def start_message(self):
         pass
 
-    def check_users_blood_type(self):
+    def get_user_blood_type(self):
         pass
 
-    def check_username(self):
+    def get_user_name(self):
         # TODO: Optional, users may be unwilling to give up personal information
         pass
 
-    def check_location(self):
+    def get_user_location(self):
         # TODO: Optional, users may be unwilling to give up personal information
         pass
 
@@ -51,3 +67,6 @@ class TelegramBot:
         notification_text = f'{bloodtype} is low - we need YOU to save lives'
         incentive_text = 'Short reminder: Blood donation will give you 2 days off and a financial remuneration'
         pass
+
+
+parser = Parser('http://kmck.kiev.ua/', None).parse_the_page()
